0b18b1236e90212acb248936fcab03fb4f368217
Matt Pahuta <mhpahuta@gmail.com>
2020-02-09 14:31:02 -0800

    Add initial randomness functionality

--- color-game.js
+++ color-game.js
@@ -10,8 +10,10 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+// var pickedColor = colors[3]; // Old static code for testing
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -25,9 +27,26 @@ for (var i=0; i<squares.length; i++){
     var clickedColor = this.style.backgroundColor;
     // compare color to pickedColor
     if (clickedColor === pickedColor){
-      alert("Correct!");
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
     } else {
-      alert("Wrong!!!");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   });
+}
+
+function changeColors(color) {
+  // loop through all squares
+  for (var i=0; i<squares.length; i++){
+    // change each color to match given color
+    squares[i].style.backgroundColor = color;
+  }
+
+}
+
+// Random color picker function
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
