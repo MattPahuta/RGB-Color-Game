42cbd6d9fe35b889ac9f68efc6e74bf51e5f0d15
Matt Pahuta <mhpahuta@gmail.com>
2020-02-09 16:44:36 -0800

    add difficulty settings and functionality

--- color-game.js
+++ color-game.js
@@ -1,28 +1,51 @@
 console.log("This thing on?")
 
-var colors = generateRandomColors(6);
-
-// Old static array test code 
-// var colors = [
-//   "rgb(255, 0, 0)",
-//   "rgb(255, 255, 0)",
-//   "rgb(0, 255, 0)",
-//   "rgb(0, 255, 255)",
-//   "rgb(0, 0, 255)",
-//   "rgb(255, 0, 255)"
-// ];
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
-// var pickedColor = colors[3]; // Old static code for testing
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+// Easy button functionality
+easyBtn.addEventListener("click", function() {
+  hardBtn.classList.remove("selected")
+  easyBtn.classList.add("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i=0; i<squares.length; i++){
+    if(colors[i]){
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+});
+// Hard button functionality
+hardBtn.addEventListener("click", function() {
+  hardBtn.classList.add("selected")
+  easyBtn.classList.remove("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i=0; i<squares.length; i++){
+      squares[i].style.backgroundColor = colors[i];
+      squares[i].style.display = "block";
+  }
+});
+
 
 resetButton.addEventListener("click", function() {
   // generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   // pick new random color from array
   pickedColor = pickColor();
   // change color display to match picked color
